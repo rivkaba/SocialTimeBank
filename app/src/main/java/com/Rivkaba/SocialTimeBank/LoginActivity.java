2dd66623fdb74fdc6e3a817b23e67324fcdee8d9
Rivka <rbir359718@gmail.com>
2021-12-01 10:49:39 +0200

    register

---
+++ app/src/main/java/com/Rivkaba/SocialTimeBank/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.Rivkaba.SocialTimeBank;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void regisret(View view) {
+        EditText email=findViewById(R.id.editTextEmail);
+        EditText password =findViewById(R.id.editTextPassword);
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                               startActivity(new Intent(LoginActivity.this,WelcomeActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                        }
+                    }
+                });
+    }
+
+    public void login(View view) {
+    }
+}
---
+++ app/src/main/java/com/Rivkaba/SocialTimeBank/WelcomeActivity.java
@@ -0,0 +1,18 @@
+package com.Rivkaba.SocialTimeBank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void logout(View view) {
+    }
+}
